cfc0509aba4efaec6a793c321848f7e57a4e8041
satyaprakash-yadav <satyay9798@gmail.com>
2024-10-23 18:29:39 +0530

    feat: Creating workspace members Added

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -27,6 +27,7 @@ export const useLogout = () => {
             toast.success("logout successfully")
             router.refresh();
             queryClient.invalidateQueries({ queryKey: ["current"] });
+            queryClient.invalidateQueries({ queryKey: ["workspaces"] });
         },
         onError: () => {
             toast.error("Failed to logout")
