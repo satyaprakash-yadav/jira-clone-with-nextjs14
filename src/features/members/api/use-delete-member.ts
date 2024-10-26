0bf1a4d6b8b3f92b9299a41c28617fa4b6be08c7
satyaprakash-yadav <satyay9798@gmail.com>
2024-10-26 17:20:52 +0530

    feat: Adding a member 'delete & update' functionality Added

---
+++ src/features/members/api/use-delete-member.ts
@@ -0,0 +1,36 @@
+import { toast } from "sonner";
+import { client } from "@/lib/rpc";
+
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+type ResponseType = InferResponseType<typeof client.api.members[":memberId"]["$delete"], 200>;
+type RequestType = InferRequestType<typeof client.api.members[":memberId"]["$delete"]>;
+
+export const useDeleteMember = () => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<
+        ResponseType,
+        Error,
+        RequestType
+    >({
+        mutationFn: async ({ param }) => {
+            const response = await client.api.members[":memberId"]["$delete"]({ param });
+
+            if (!response.ok) {
+                throw new Error("Failed to delete member")
+            }
+
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Member deleted");
+            queryClient.invalidateQueries({ queryKey: ["members"] });
+        },
+        onError: () => {
+            toast.error("Failed to delete member");
+        },
+    });
+    return mutation;
+};
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -43,5 +43,8 @@ export async function createAdminClient() {
         get account() {
             return new Account(client);
         },
+        get users() {
+            return new Users(client);
+        },
     }
 }
