56d904f77369f78ac37e0ab54a215f785723c663
satyaprakash-yadav <satyay9798@gmail.com>
2024-10-28 11:55:13 +0530

    feat: Adding workspace projects Added

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,16 +2,18 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import auth from "@/features/auth/server/route";
-import workspaces from "@/features/workspaces/server/route";
 import members from "@/features/members/server/route";
+import projects from "@/features/projects/server/route";
+import workspaces from "@/features/workspaces/server/route";
 
 const app = new Hono().basePath("/api");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
     .route("/auth", auth)
-    .route("/workspaces", workspaces)
     .route("/members", members)
+    .route("/projects", projects)
+    .route("/workspaces", workspaces)
 
 export const GET = handle(app);
 export const POST = handle(app);
