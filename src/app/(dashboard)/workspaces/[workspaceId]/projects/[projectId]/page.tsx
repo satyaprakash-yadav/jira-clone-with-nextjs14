9a16e8f12b516f5ab4a85dba08defa57c368445e
satyaprakash-yadav <satyay9798@gmail.com>
2024-10-30 10:49:10 +0530

    feat: Building a tasks API Added

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -5,6 +5,7 @@ import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { PencilIcon } from "lucide-react";
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
 
 interface ProjectIdPageProps {
   params: { projectId: string };
@@ -44,6 +45,7 @@ const ProjectIdPage = async ({ params }: ProjectIdPageProps) => {
           </Button>
         </div>
       </div>
+      <TaskViewSwitcher />
     </div>
   );
 };
--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -5,6 +5,7 @@ import auth from "@/features/auth/server/route";
 import members from "@/features/members/server/route";
 import projects from "@/features/projects/server/route";
 import workspaces from "@/features/workspaces/server/route";
+import tasks from "@/features/tasks/server/route";
 
 const app = new Hono().basePath("/api");
 
@@ -14,6 +15,7 @@ const routes = app
     .route("/members", members)
     .route("/projects", projects)
     .route("/workspaces", workspaces)
+    .route("/tasks", tasks)
 
 export const GET = handle(app);
 export const POST = handle(app);
