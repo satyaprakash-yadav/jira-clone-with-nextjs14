d1da93fcad399af1ccb71f046a3b1ea3aef18bfd
satyaprakash-yadav <satyay9798@gmail.com>
2024-11-04 12:33:46 +0530

    feat: Building a data table Added

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -13,6 +13,8 @@ import { useGetTasks } from "../api/use-get-tasks";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
 
 export const TaskViewSwitcher = () => {
   const [view, setView] = useQueryState("task-view", {
@@ -22,7 +24,7 @@ export const TaskViewSwitcher = () => {
 
   const workspaceId = useWorkspaceId();
   const { open } = useCreateTaskModal();
-  const { data: task, isLoading: isLoadingTask } = useGetTasks({ 
+  const { data: tasks, isLoading: isLoadingTask } = useGetTasks({
     workspaceId,
     projectId,
     assigneeId,
@@ -64,13 +66,13 @@ export const TaskViewSwitcher = () => {
         ) : (
           <>
             <TabsContent value="table" className="mt-0">
-              {JSON.stringify(task)}
+              <DataTable columns={columns} data={tasks?.documents ?? []} />
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
-              {JSON.stringify(task)}
+              {JSON.stringify(tasks)}
             </TabsContent>
             <TabsContent value="calender" className="mt-0">
-              {JSON.stringify(task)}
+              {JSON.stringify(tasks)}
             </TabsContent>
           </>
         )}
